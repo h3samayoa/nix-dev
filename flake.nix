da3b24ff89d476edc0d4794c455e6b9bd97d5535
h3samayoa <h3samayoa@gmail.com>
2024-06-07 14:45:50 -0700

    added skeleton for terraform flake file

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "nix devenvs for projs"
+
+  #define packages to use // can also format as i.e: inputs.nixpkgs.url = "" // 
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-23.11";
+    };
+
+    devenv = {
+      url = "github:cachix/devenv";
+    };
+
+    nixpkgs-terraform = { #specific for terraform projects this can be omitted
+      url = "github:stackbuilders/nixpkgs-terraform";
+    };
+
+    # the systems you want to expose are defined here i.e for only x64 use github:nix-systems/x86_64-linux // more info: https://github.com/nix-systems/nix-systems
+    systems = { 
+      url = "github:nix-systems/default";
+    };
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, nixpkgs-terraform, systems }@inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [];
+            };
+          }    
+        );
+    };
+}
